7b282f0137d511928d4eedf7ad1bb8273872a7e4
student <steven1203@naver.com>
2016-07-11 14:21:30 +0900

    commit test

---
+++ app/src/main/java/com/example/student/git3/Git3Activity.java
@@ -0,0 +1,14 @@
+package com.example.student.git3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Git3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git3);
+        //adsflkajdslkfajlfdsk
+    }
+}
